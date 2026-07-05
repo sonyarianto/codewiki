adbf742ea05929be0a6c28440520c8ec9cccbcd7
Sony AK <sony@sony-ak.com>
2026-07-05 12:46:23 +0700

    docs: fix copyright year to 2026

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
 
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2024-present',
+      copyright: 'Copyright © 2026-present',
     },
   },
 })
